d109fb6ad4357ac111771e06d75b3f0051bd58c5
hjetschko <hubert@robospace.co.nz>
2025-06-15 14:23:26 +1200

    Delete previous twister out directory

--- tests/twister_ncs_2.9.0.bat
+++ tests/twister_ncs_2.9.0.bat
@@ -2,6 +2,6 @@ IF "%~1"=="" (
     echo Error: No COM port specified.
     exit /b 1
 )
-
+rmdir /s /q twister-out
 SET PATH=C:\ncs\toolchains\b620d30767\opt\bin;C:\ncs\toolchains\b620d30767\opt\bin\Scripts;%PATH%
 python C:\ncs\v2.9.0\zephyr\scripts\twister -T sleep_scheduler_test -p nrf52840dk/nrf52840 --device-testing --device-serial %1
